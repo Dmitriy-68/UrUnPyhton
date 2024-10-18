3d61befa76afe970f7b81effb5e02887b6887cea
Dmitriy-68 <d73-68@yandex.ru>
2024-10-18 11:32:19 +0300

    Модуль 12 Задание 1 Простые Юнит-Тесты

--- module12/tests_12_1.py
+++ module12/tests_12_1.py
@@ -4,20 +4,20 @@ import runner
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        runner1 = runner.Runner('Halk')
+        runner1 = runner.Runner('Халк')
         for i in range(10):
             runner1.walk()
         self.assertEqual(runner1.distance, 50)
 
     def test_run(self):
-        runner1 = runner.Runner('Halk')
+        runner1 = runner.Runner('Халк')
         for i in range(10):
             runner1.run()
         self.assertEqual(runner1.distance, 100)
 
     def test_challenge(self):
-        runner1 = runner.Runner('Halk')
-        runner2 = runner.Runner('Thor')
+        runner1 = runner.Runner('Халк')
+        runner2 = runner.Runner('Тор')
         for i in range(10):
             runner1.run()
             runner2.walk()
