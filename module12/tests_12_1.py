f6fa02b512614ed6ec1160d8d4d677fb720a18ff
Dmitriy-68 <d73-68@yandex.ru>
2024-10-18 11:29:39 +0300

    Модуль 11 Задание 2 Интроспекция

---
+++ module12/tests_12_1.py
@@ -0,0 +1,28 @@
+import unittest
+import runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner1 = runner.Runner('Halk')
+        for i in range(10):
+            runner1.walk()
+        self.assertEqual(runner1.distance, 50)
+
+    def test_run(self):
+        runner1 = runner.Runner('Halk')
+        for i in range(10):
+            runner1.run()
+        self.assertEqual(runner1.distance, 100)
+
+    def test_challenge(self):
+        runner1 = runner.Runner('Halk')
+        runner2 = runner.Runner('Thor')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
