9fb7703fec0244cc502b2498ad46f990d7b31790
Dmitriy-68 <d73-68@yandex.ru>
2024-11-30 07:29:22 +0300

    Модуль 16 Задание 3 Имитация работы с БД

--- module16/module_16_3.py
+++ module16/module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -11,19 +11,24 @@ async def get_all_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def register_user(username: str, age: int) -> str:
+async def register_user(username: str = Path(min_length=5, max_length=20, description='Enter username',
+                                           example='UrbanUser'),
+                        age: int = Path(ge=18, le=120, description='Enter age', example='24')) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} is registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> str:
+async def update_user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='17'),
+                      username: str = Path(min_length=5, max_length=20, description='Enter username',
+                                           example='UrbanUser'),
+                      age: int = Path(ge=18, le=120, description='Enter age', example='24')) -> str:
     users[str(user_id)] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> str:
+async def delete_user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='17')) -> str:
     users.pop(str(user_id))
     return f'User {user_id} has been deleted'
